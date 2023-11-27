794093544fbcac57aa22433052a3e2424c04c949
maherBTA <maher.ben@foton-it.com>
2023-11-27 09:59:21 +0100

    UBUNTU: Remove multimedia codecs from kernel

---
+++ debian/linux-headers-5.15.0-52/usr/src/linux-headers-5.15.0-52/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Sat Nov 25 13:35:32 CET 2023"
+#define LINUX_COMPILE_BY "shaco"
+#define LINUX_COMPILE_HOST "shaco"
+#define LINUX_COMPILER "gcc (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
